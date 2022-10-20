b091acce45229575ce6fa4706b114c655dc552da
JennyFletch <111204164+JennyFletch@users.noreply.github.com>
2022-10-20 11:52:41 -0600

    Added 5-day forecast to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,9 +23,17 @@ var openweatherKey = 'bdb4743787713bf16cafd1c2dec3eb6a';
 // doc link: https://openweathermap.org/api/geocoding-api#direct
 
 
+// Get today's date from day.js
+var dateMonth = dayjs().month();
+var dateDay = dayjs().date();
+var dateYear = dayjs().year();
+var dateToday = dateMonth + "/" + dateDay + "/" + dateYear;
+
 // GLOBAL VARIABLES
 var submitBtnEl = document.getElementById("btn-submit");
 var cityInputEl = document.getElementById("city-input");
+var forecastTodayEl = document.getElementById("forecast-today");
+var forecastFivedayEl = document.getElementById("forecast-fiveday");
 var cityName;
 var cityShortcutsEl = document.getElementById("city-shortcuts");
 var cityShortcuts = []; // array to hold city names saved to localStorage
@@ -48,8 +56,8 @@ function getTheWeather(cityName) {
     .then(function (data) {
         //console.log("in getApi function: " + data[0].lat);
         // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-        var weatherTodayUrl = openweatherBaseUrl + 'data/2.5/weather?lat=' + data[0].lat + '&lon=' + data[0].lon + '&appid=' + openweatherKey;
-        var weatherForecastUrl = openweatherBaseUrl + 'data/2.5/forecast/daily?lat=' + data[0].lat + '&lon=' + data[0].lon + '&cnt=5&appid=' + openweatherKey;
+        var weatherTodayUrl = openweatherBaseUrl + 'data/2.5/weather?lat=' + data[0].lat + '&lon=' + data[0].lon + '&units=imperial&appid=' + openweatherKey;
+        var weatherForecastUrl = openweatherBaseUrl + 'data/2.5/forecast/daily?lat=' + data[0].lat + '&lon=' + data[0].lon + '&units=imperial&cnt=5&appid=' + openweatherKey;
 
         // Get today's weather in the selected city
         fetch(weatherTodayUrl)
@@ -62,7 +70,25 @@ function getTheWeather(cityName) {
               }
         })
         .then(function (owWeather) {
+            // Build display of today's weather
             console.log(owWeather);
+
+            var cityDateAndIcon = document.createElement('h3');
+            cityDateAndIcon.textContent = cityName + " (" + dateToday + ")";
+            var weatherIcon = document.createElement('img');
+            var iconUrl = "https://openweathermap.org/img/wn/" + owWeather.weather[0].icon + "@2x.png";
+            weatherIcon.setAttribute("src", iconUrl);
+            cityDateAndIcon.appendChild(weatherIcon);
+            forecastTodayEl.appendChild(cityDateAndIcon);
+            var dataTemp = document.createElement("p");
+            dataTemp.textContent = "Temp: " + owWeather.main.temp + "\u00B0F";
+            forecastTodayEl.appendChild(dataTemp);
+            var dataWind = document.createElement("p");
+            dataWind.textContent = "Wind: " + owWeather.wind.speed + "MPH";
+            forecastTodayEl.appendChild(dataWind);
+            var dataHumidity = document.createElement("p");
+            dataHumidity.textContent = "Humidity: " + owWeather.main.humidity + "%";
+            forecastTodayEl.appendChild(dataHumidity);
         });
 
         // Get 5-day forecast in the selected city
@@ -76,7 +102,37 @@ function getTheWeather(cityName) {
               }
         })
         .then(function (owForecast) {
+            // Build display of 5-day forecast
             console.log(owForecast);
+            var forecastTitle = document.createElement("h4");
+            forecastTitle.textContent = "5-Day Forecast:";
+            forecastFivedayEl.appendChild(forecastTitle);
+            var forecastList = document.createElement("ul");
+            forecastFivedayEl.appendChild(forecastList);
+            for (var i=0; i<5; i++) {
+                var listItem = document.createElement("li");
+                //listItem.textContent = "List Item #" + i;
+                forecastList.appendChild(listItem);
+                var forecastTitle = document.createElement("h5");
+                var forecastDate = dateDay + i + 1;
+                var liTitle = dateMonth + "/" + forecastDate + "/" + dateYear;
+                forecastTitle.textContent = liTitle;
+                listItem.appendChild(forecastTitle);
+                var forecastIcon = document.createElement("img");
+                var iconUrl = "https://openweathermap.org/img/wn/" + owForecast.list[i].weather[0].icon + "@2x.png";
+                forecastIcon.setAttribute("src", iconUrl);
+                listItem.appendChild(forecastIcon);
+                // temp, wind, humidity
+                var forecastTemp = document.createElement("p");
+                forecastTemp.textContent = "Temp: " + owForecast.list[i].temp.day + "\u00B0F";
+                listItem.appendChild(forecastTemp);
+                var forecastWind = document.createElement("p");
+                forecastWind.textContent = "Wind: " + owForecast.list[i].gust + "MPH";
+                listItem.appendChild(forecastWind);
+                var forecastHumidity = document.createElement("p");
+                forecastHumidity.textContent = "Humidity: " + owForecast.list[i].humidity + "%";
+                listItem.appendChild(forecastHumidity);
+            }
         });
         
     }); 
@@ -84,7 +140,7 @@ function getTheWeather(cityName) {
 
 function makeCityButton(cityName) {
     var cityBtn = document.createElement('li');
-    cityBtn.setAttribute('data-location', cityName);
+    // cityBtn.setAttribute('data-location', cityName);
     cityBtn.textContent = cityName;
     cityShortcutsEl.appendChild(cityBtn);
 }
